ba95bd46f390c5a81b73b33e65ffde6059e76933
Rodrez <fabian.rodrez@gmail.com>
2022-07-21 23:35:34 -0500

    Added span to have better logging visibility

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::web::Form;
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -14,16 +15,32 @@ pub async fn subscribe(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpRes
     // Unique Identifier to trace issues
     let request_id = Uuid::new_v4();
 
-    tracing::info!(
-        "request_id {} -> Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    // tracing::info!(
+    //     "request_id {} -> Adding '{}' '{}' as a new subscriber.",
+    //     request_id,
+    //     form.email,
+    //     form.name
+    // );
+    // tracing::info!(
+    //     "request_id {} ->Saving new subscriber details in the database",
+    //     request_id
+    // );
+
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the level info
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    tracing::info!(
-        "request_id {} ->Saving new subscriber details in the database",
-        request_id
-    );
+
+    // Using enter for async function is not good.
+    let _request_span_guard = request_span.enter();
+
+    // We do not use `.enter` as `instrument` takes care of
+    // it at the right time
+    let query_span = tracing::info_span!("Saving new subscriber details in database.");
 
     match sqlx::query!(
         r#"
@@ -36,6 +53,7 @@ pub async fn subscribe(form: Form<FormData>, pool: web::Data<PgPool>) -> HttpRes
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
