eac6b71f5d57e3cefd30cc55bc09bb3ed69ddd94
Fabian <fabian.rodrez@gmail.com>
2022-07-27 00:30:41 -0500

    Removed refrence in main.rs which was unnecesary

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ async fn main() -> std::io::Result<()> {
     // Exposed the secret
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .connect_lazy(configuration.database.connection_string().expose_secret())
         .expect("Failed to connect to postgres");
 
     let address = format!(
