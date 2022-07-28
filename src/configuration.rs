7dc32d01903733e2b60490e3bf219b3f9ae09448
Fabian <fabian.rodrez@gmail.com>
2022-07-28 08:22:30 -0500

    Adjusted the add source for Environment to reflect the prefix separator as `_` and the separator as `__`

--- src/configuration.rs
+++ src/configuration.rs
@@ -77,7 +77,11 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         ))
         // Add in settings from environment variables (with a prefix of APP and '__' as separator)
         // E.g. `APP_APPLICATION__PORT=5001` would set "Settings.application.port"
-        .add_source(config::Environment::with_prefix("APP").separator("__"))
+        .add_source(
+            config::Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()?;
 
     settings.try_deserialize::<Settings>()
