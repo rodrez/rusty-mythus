e96a635b961f65e5f5de2c4886263a5b34090aac
Fabian <fabian.rodrez@gmail.com>
2022-09-19 15:14:34 -0500

    FEATURE: Added new migration
    The migration fix the problem with missing columns

--- migrations/20220914171725_add_extra_fields_to_jobs_table.sql
+++ migrations/20220914171725_add_extra_fields_to_jobs_table.sql
@@ -1,6 +1,6 @@
 -- Add migration script here
 ALTER TABLE jobs 
-ADD COLUMN agreement BOOLEAN NOT NULL,
+ADD agreement BOOLEAN NOT NULL,
 ADD first_name TEXT NOT NULL,
 ADD last_name TEXT NOT NULL,
 ADD company_size TEXT NOT NULL,
