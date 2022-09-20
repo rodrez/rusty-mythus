0d2985bc554c8e33d19731d5e97bda329f4bd28a
Fabian <fabian.rodrez@gmail.com>
2022-09-19 22:13:21 -0500

    Updated spec for do

--- migrations/20220914171725_add_extra_fields_to_jobs_table.sql
+++
@@ -1,7 +0,0 @@
--- Add migration script here
-ALTER TABLE jobs 
-ADD agreement BOOLEAN NOT NULL,
-ADD first_name TEXT NOT NULL,
-ADD last_name TEXT NOT NULL,
-ADD company_size TEXT NOT NULL,
-ADD phone_number TEXT NOT NULL;
